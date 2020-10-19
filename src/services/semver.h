244eb88de734adaf7509c3c82145b28baccbe323
Malachi Burke <malachi.burke@gmail.com>
2020-10-18 22:40:13 -0700

    Prep

---
+++ src/services/semver.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace moducom {
+
+struct SemVer
+{
+    const unsigned short major;
+    const unsigned short minor;
+    const unsigned short patch;
+
+    const char* prerelease;
+};
+
+}
--- src/services/service.hpp
+++ src/services/service.hpp
@@ -1,7 +1,58 @@
 #pragma once
 
+#include "semver.h"
+#include <utility>
+
 namespace moducom { namespace services {
 
+class Manager
+{
 
+};
+
+
+class Description
+{
+    const char* name_;
+    const SemVer version_;
+
+public:
+    Description(char* name, SemVer version) :
+        name_(name), version_(version)
+    {}
+};
+
+namespace agents {
+
+template <class TService>
+class Base
+{
+    std::aligned_storage<sizeof(TService), alignof(TService)> raw;
+
+protected:
+    typedef TService service_type;
+    typedef service_type& reference;
+
+    reference service()
+    {
+        return reinterpret_cast<reference>(raw);
+    }
+};
+
+template <class TService>
+class SingleShot : public Base<TService>
+{
+
+public:
+};
+
+
+template <class TService>
+class Periodic : public Base<TService>
+{
+
+};
+
+}
 
 }}
