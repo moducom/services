14fc2a2c12a1ba3ae13100a8a349e007f4203c30
Malachi Burke <malachi.burke@gmail.com>
2020-10-18 22:27:35 -0700

    More boilerplate-class stuff

---
+++ src/services.tests/misc.cpp
@@ -0,0 +1,12 @@
+#include "catch2/catch.hpp"
+#include <service.hpp>
+
+using namespace moducom::services;
+
+TEST_CASE("misc")
+{
+    SECTION("A")
+    {
+
+    }
+}
---
+++ src/services/service.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace moducom { namespace services {
+
+
+
+}}
