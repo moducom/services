b1baf51c8f61dac88b597b0e981a36fc03748997
Malachi Burke <malachi.burke@gmail.com>
2020-11-15 22:12:12 -0800

    Prepping libusb service

---
+++ src/service.libusb/usb.cpp
@@ -0,0 +1,9 @@
+#include "usb.h"
+
+#include <libusb-1.0/libusb.h>
+
+namespace moducom { namespace services {
+
+
+
+}}
---
+++ src/service.libusb/usb.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace moducom { namespace services {
+
+
+}}
