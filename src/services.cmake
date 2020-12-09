e01986f8379ce4208b5dfc78f1845404cab3df45
Malachi Burke <malachi.burke@gmail.com>
2020-12-09 07:55:32 -0800

    Experimenting with this to let entt install on a vs2017 environment
    See https://github.com/skypjack/entt/issues/606

--- src/services.cmake
+++ src/services.cmake
@@ -1,5 +1,7 @@
 set(ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/..)
 
+set(CONAN_DISABLE_CHECK_COMPILER 1)
+
 if(NOT DEFINED CONAN_LIB_DIRS)
     include(${ROOT_DIR}/../lib/conan/conanbuildinfo.cmake)
     conan_basic_setup(TARGETS KEEP_RPATHS)
