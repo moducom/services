73db13910b5413c625966ccfcb979ec0ea393361
Malachi Burke <malachi.burke@gmail.com>
2020-12-09 21:22:35 -0800

    Reworking for conan cmake_find_package generator

--- src/services.cmake
+++ src/services.cmake
@@ -1,8 +1,12 @@
-set(ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/..)
+set(ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/..)
 
-if(NOT DEFINED CONAN_LIB_DIRS)
-    include(${ROOT_DIR}/../lib/conan/conanbuildinfo.cmake)
-    conan_basic_setup(TARGETS KEEP_RPATHS)
-endif()
+set(CMAKE_MODULE_PATH ${ROOT_DIR}/lib/conan)
+
+#if(NOT DEFINED CONAN_LIB_DIRS)
+#    include(${ROOT_DIR}/../lib/conan/conanbuildinfo.cmake)
+#    conan_basic_setup(TARGETS KEEP_RPATHS)
+#endif()
 
 find_package(Threads REQUIRED)
+find_package(libusb REQUIRED)
+find_package(EnTT)
