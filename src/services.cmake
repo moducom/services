4090e9936a539d6bfee13045c03a385f853a0f53
Malachi Burke <malachi.burke@gmail.com>
2020-12-10 15:10:15 -0800

    More gracefully specifying CMAKE_MODULE_PATH

--- src/services.cmake
+++ src/services.cmake
@@ -1,12 +1,8 @@
 set(ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/..)
 
-set(CMAKE_MODULE_PATH ${ROOT_DIR}/lib/conan)
-
-#if(NOT DEFINED CONAN_LIB_DIRS)
-#    include(${ROOT_DIR}/../lib/conan/conanbuildinfo.cmake)
-#    conan_basic_setup(TARGETS KEEP_RPATHS)
-#endif()
+# Passively specify where our modules live
+list(APPEND CMAKE_MODULE_PATH ${ROOT_DIR}/lib/conan)
 
 find_package(Threads REQUIRED)
-find_package(libusb REQUIRED)
+find_package(libusb 1.0.23 REQUIRED)
 find_package(EnTT REQUIRED)
