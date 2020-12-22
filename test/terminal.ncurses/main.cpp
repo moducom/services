50bdb5aa8843593c69049413036069f38954d39e
Malachi Burke <malachi.burke@gmail.com>
2020-12-21 22:28:40 -0800

    Bit by bit

--- test/terminal.ncurses/main.cpp
+++ test/terminal.ncurses/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
 #include <ncurses.h>
+#include <moducom/libusb/service.hpp>
+#include "../../src/service.libusb/acm.h"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    moducom::services::LibUsb libusb;
+    
+    initscr();
+    printw("Hello World !!!");
+    refresh();
+    getch();
+    endwin();
+    //std::cout << "Hello, World!" << std::endl;
     return 0;
 }
