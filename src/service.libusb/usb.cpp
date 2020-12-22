f2482fe37f9d1ab5ddf276ed1a3f77b4a0f61d52
Malachi Burke <malachi.burke@gmail.com>
2020-12-22 00:06:10 -0800

    Adding service-level device struct

--- src/service.libusb/usb.cpp
+++ src/service.libusb/usb.cpp
@@ -3,6 +3,38 @@
 
 namespace moducom { namespace services {
 
+// TODO: Fix this lousy name
+struct LibUsbMeta
+{
+    libusb::Device device;
+    libusb_device_descriptor device_descriptor;
+    //libusb::ConfigDescriptor config;
+
+    LibUsbMeta(libusb::Device device) :
+        device(device)
+    {
+        device.ref();
+
+        libusb_get_device_descriptor(device, &device_descriptor);
+    }
+
+    ~LibUsbMeta()
+    {
+        device.unref();
+    }
+
+    //LibUsbMeta(const LibUsbMeta&) = default;
+
+    LibUsbMeta(LibUsbMeta&&) = default;
+
+    LibUsbMeta& operator=(const LibUsbMeta& copyFrom)
+    {
+        new (&device) libusb::Device(copyFrom.device);
+        device_descriptor = copyFrom.device_descriptor;
+        return *this;
+    }
+};
+
 inline void LibUsb::add_device(libusb_device* device)
 {
     libusb::Device d(device);
@@ -47,6 +79,8 @@ inline void LibUsb::add_device(libusb_device* device)
         libusb_get_descriptor(device, LIBUSB_DT_INTERFACE, 0, &interface_descriptor,
             LIBUSB_DT_INTERFACE_SIZE); */
     }
+
+    registry.emplace<LibUsbMeta>(id, device);
 }
 
 inline void LibUsb::remove_device(libusb::Device d, entt::entity deviceId)
